b0c549bdb9665349cc1add0510c970e88b3b0796
nidaonder <nidaaonder@gmail.com>
2023-09-22 00:04:20 +0300

    Yorum satırları eklendi.

--- MaasHesaplama/src/Employee.java
+++ MaasHesaplama/src/Employee.java
@@ -11,22 +11,22 @@ public class Employee {
         this.hireYear = hireYear;
     }
     double tax() {
-        if (this.salary < 1000) {
+        if (this.salary < 1000) {       // Maaş 1000'den az ise vergi 0.
             return 0;
-        } else {
+        } else {               // Maaş 1000'den çok ise vergi %30.
             return this.salary * 0.03;
         }
     }
 
     double bonus () {
-        if (this.workHours <= 40) {
+        if (this.workHours <= 40) {    // Mesai saatine göre hesaplama yapıldı
             return 0;
         } else {
             return (this.workHours - 40) * 30;
         }
     }
 
-    double raiseSalary() {
+    double raiseSalary() {                   // Çalışanın şirketteki yılına göre zam oranı hesaplandı.
         if (2021 - this.hireYear < 10) {
            return (this.salary * 0.05);
         } else if (2021 - this.hireYear > 10 && 2021 - this.hireYear < 20) {
