1bddcf83047ad1cc7023bb1e30917119ba14874b
nidaonder <nidaaonder@gmail.com>
2023-09-24 16:46:24 +0300

    Matrisin transpozunu bulan program eklendi.

---
+++ TransposeOfMatrix/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Main {
+
+    static void printMatrix(int[][] arr) {   // Matrisleri ekrana yazdırmak için metod oluşturdum.
+
+        for (int i=0; i < arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {   // 2 boyutlu matris dizisi oluşturdum.
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        printMatrix(matrix);
+
+        int[][] transpose = new int[matrix[0].length][matrix.length];  // matrisin transpozunu for döngüsü kullanarak buldum.
+        for (int j = 0; j<matrix[0].length; j++){
+            for (int i = 0; i< matrix.length; i++){
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        printMatrix(transpose);
+    }
+}
